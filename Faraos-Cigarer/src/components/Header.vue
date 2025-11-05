36d1ab85efb43bff45d9127a8ab03ee17b442cfc
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-11-05 12:59:56 +0100

    Vue

--- Faraos-Cigarer/src/components/Header.vue
+++ Faraos-Cigarer/src/components/Header.vue
@@ -1,21 +1,29 @@
 <script setup></script>
 
 <template>
-  <header class="header">
-    <h1>Faraos Cigarer</h1>
+  <header>
+    <div class="header">
+    <h1 class="header__logo">Faraos Cigarer</h1>
+    <input class="header__search" type="text" placeholder="søg" />
+    </div>
   </header>
 </template>
 
 <style scoped>
-body {
+.header{
     display: flex;
+    flex-wrap: wrap;
 }
 
-header {
+.header__logo {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100px;
     font-family: 'Boogaloo';
+}
+
+.search__header {
+    display: flex;
+    justify-content: center;
 }
 </style>
