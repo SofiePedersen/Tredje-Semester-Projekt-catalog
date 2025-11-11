e0b8999f1d4b7eafa5b5655a70302b1429ad253a
madsmads4 <madsolsen213@gmail.com>
2025-11-11 13:03:48 +0100

    Update ContactComponent.vue

--- Faraos-Cigarer/src/components/ContactComponent.vue
+++ Faraos-Cigarer/src/components/ContactComponent.vue
@@ -3,7 +3,17 @@
 <template>
   <div class="aabningstider">
     <p class="aabningstider__titel"></p>
-    <p class="aabningstider__titel"></p>
+    <p class="aabningstider__text"></p>
+  </div>
+
+  <div class="kontakt">
+    <p class="kontakt__titel"></p>
+    <p class="kontakt__text"></p>
+  </div>
+
+  <div class="lukke-dage">
+    <p class="lukke-dage__titel"></p>
+    <p class="lukke-dage__text"></p>
   </div>
 </template>
 
