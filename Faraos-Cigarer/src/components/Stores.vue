dd6f57cf729a9d1b6f68ed2c3a49e0a85bea788f
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-11-12 11:07:18 +0100

    Update Stores.vue

--- Faraos-Cigarer/src/components/Stores.vue
+++ Faraos-Cigarer/src/components/Stores.vue
@@ -43,7 +43,10 @@ function toggleSection(id) {
 
 
 <template>
-  <img class="butikker__kort" src="@/assets/image/Landekort.webp" alt="Landkort" />
+  <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d577050.2589892185!2d10.37101225287178!3d55.60022845645195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sFaraos%20Cigarer!5e0!3m2!1sda!2sdk!4v1762941964703!5m2!1sda!2sdk" 
+  width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+  </iframe>
+  
   <h2>HVOR VIL DU HEN?</h2>
 
   <div v-for="section in storeMapSections" :key="section.id" class="butikker__sektion" @click="toggleMenu">
