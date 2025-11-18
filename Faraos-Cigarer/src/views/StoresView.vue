b0673ac4f37ed08f43d016ba97af187a71cca0d5
madsmads4 <madsolsen213@gmail.com>
2025-11-18 11:01:09 +0100

    done

--- Faraos-Cigarer/src/views/StoresView.vue
+++ Faraos-Cigarer/src/views/StoresView.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import Stores from '@/components/Stores.vue';
-
+import Stores from "@/components/Stores.vue";
+import StoresDesktop from "@/components/StoresDesktop.vue";
 </script>
 
 <template>
   <Stores></Stores>
+  <StoresDesktop></StoresDesktop>
 </template>
 
-<style>
-
-</style>
+<style></style>
